7b054d7ac9fc1a290f15a656e6f4ef3492bef698
Seans Dowdle <dowdle_sean@live.com>
2018-07-11 22:47:57 -0700

    Nearly completed functionality for GenerateRoutinePane

--- IndecisiveGainz/src/indecisivegainz/view/GenerateRoutinePane.java
+++ IndecisiveGainz/src/indecisivegainz/view/GenerateRoutinePane.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +15,7 @@ import indecisivegainz.controller.Controller;
 import javafx.event.ActionEvent;
 
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
 public class GenerateRoutinePane implements Initializable
 {
@@ -25,6 +27,10 @@ public class GenerateRoutinePane implements Initializable
 	private Button clearButton;
 	@FXML
 	private GridPane gridPane;
+	@FXML
+	private Label statusMessage;
+	@FXML
+	private Button forwardButton;
 	
 	private ComboBox<String>[] comboBoxes = (ComboBox<String>[]) new ComboBox[5];
 	private TextField[] textFields = new TextField[5];
@@ -36,33 +42,69 @@ public class GenerateRoutinePane implements Initializable
 	@FXML
 	public void generateRoutineButton() 
 	{
-		
+		boolean isSuccess = true;
+		for(int i = 0; i < numMenuItems; i++)
+		{
+			if(comboBoxes[i].getSelectionModel().getSelectedIndex() != -1 && !textFields[i].getText().equals(""))
+			{
+				statusMessage.setText("Routine Generated Successfully");
+				statusMessage.setTextFill(Color.GREEN);
+			}
+			else
+			{
+				isSuccess = false;
+				i = numMenuItems;
+			}
+		}
+		if(isSuccess)
+		{
+			statusMessage.setText("Routine Generated Successfully");
+			statusMessage.setTextFill(Color.GREEN);
+			
+			forwardButton.setDisable(false);
+			forwardButton.setVisible(true);
+		}
+		else
+		{
+			statusMessage.setText("Error Generating Routine. Check Fields.");
+			statusMessage.setTextFill(Color.RED);
+			
+			forwardButton.setDisable(true);
+			forwardButton.setVisible(false);
+		}
 	}
 	// Event Listener on Button[#clearButton].onAction
 	@FXML
 	public void clearFields() 
 	{
-		for(int i = 0; i < numMenuItems; i++)
+		comboBoxes[0].getSelectionModel().select(-1);
+		textFields[0].clear();
+		
+		for(int i = 1; i < numMenuItems; i++)
 		{
 			gridPane.getChildren().remove(comboBoxes[i]);
 			gridPane.getChildren().remove(textFields[i]);
+			gridPane.getChildren().remove(buttons[i]);
 			comboBoxes[i] = null;
 			textFields[i] = null;
 			buttons[i] = null;
 		}
 		
 		numMenuItems = 1;
-		Button b = createAddItemButton();
-		buttons[numMenuItems - 1] = b;
-		gridPane.add(b, 0, 2);
+		if(!gridPane.getChildren().contains(buttons[0]))
+			gridPane.add(buttons[0], 0, 2);
+		
+		statusMessage.setText("");
+		forwardButton.setDisable(true);
+		forwardButton.setVisible(false);
 	}
 	
 	public ComboBox<String> createMuscleGroupsCB()
 	{
 		ComboBox<String> cb = new ComboBox<String>();
-		cb.setPrefHeight(50);
+		cb.setPrefHeight(40);
 		cb.setPrefWidth(160);
-		cb.setMaxHeight(50);
+		cb.setMaxHeight(40);
 		cb.setMaxWidth(160);
 		cb.setPromptText("Select Muscle Group");
 		cb.setItems(controller.getAllMuscleGroups());
@@ -73,10 +115,10 @@ public class GenerateRoutinePane implements Initializable
 	public TextField createNumRepsTF()
 	{
 		TextField tf = new TextField();
-		tf.setPrefWidth(50);
-		tf.setPrefHeight(50);
-		tf.setMaxWidth(50);
-		tf.setMaxHeight(50);
+		tf.setPrefWidth(40);
+		tf.setPrefHeight(40);
+		tf.setMaxWidth(40);
+		tf.setMaxHeight(40);
 		
 		return tf;
 	}
@@ -85,10 +127,10 @@ public class GenerateRoutinePane implements Initializable
 	{
 		Button button = new Button();
 		button.setText("+");
-		button.setPrefWidth(50);
-		button.setPrefHeight(50);
-		button.setMaxWidth(50);
-		button.setMaxHeight(50);
+		button.setPrefWidth(40);
+		button.setPrefHeight(40);
+		button.setMaxWidth(40);
+		button.setMaxHeight(40);
 		button.setOnAction(e -> addMenuItem());
 		
 		return button;
@@ -98,13 +140,12 @@ public class GenerateRoutinePane implements Initializable
 	{
 		if(numMenuItems < 5)
 		{
-			// TODO Delete the addItem button at the current index before adding new item.
 			ComboBox<String> cb = createMuscleGroupsCB();
-			comboBoxes[numMenuItems - 1] = cb;
+			comboBoxes[numMenuItems] = cb;
 			gridPane.add(cb, 0, numMenuItems + 1);
 			
 			TextField tf = createNumRepsTF();
-			textFields[numMenuItems - 1] = tf;
+			textFields[numMenuItems] = tf;
 			gridPane.add(tf, 1, numMenuItems + 1);
 			
 			gridPane.getChildren().remove(buttons[numMenuItems - 1]);
@@ -119,15 +160,30 @@ public class GenerateRoutinePane implements Initializable
 			}
 		}
 	}
+	
+	@FXML
+	public void loadViewGeneratedRoutinePane()
+	{
+		// TODO ViewNavigator.loadPane("");
+	}
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
 	{
 		// Column then Row
-		gridPane.add(createMuscleGroupsCB(), 0, 1);
-		gridPane.add(createNumRepsTF(), 1, 1);
+		ComboBox<String> c = createMuscleGroupsCB();
+		comboBoxes[numMenuItems] = c;
+		gridPane.add(c, 0, 1);
+		
+		TextField t = createNumRepsTF();
+		textFields[numMenuItems] = t;
+		gridPane.add(t, 1, 1);
+		
 		Button b = createAddItemButton();
 		buttons[numMenuItems] = b;
 		gridPane.add(b, 0, 2);
 		numMenuItems++;
+		
+		forwardButton.setDisable(true);
+		forwardButton.setVisible(false);
 	}
 }
